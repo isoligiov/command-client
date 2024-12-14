01375077edc4279278dfe311b5d071ba18dc39e9
mac <mac@macs-MacBook-Pro-3.local>
2024-12-14 03:49:17 -0500

    ws connection

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
-import urllib.request
-import urllib.parse
-import sys
+import websocket
+import ssl
+import rel
 import json
 
-SERVER_URL = 'http://5.133.9.244:10000'
+SERVER_URL = 'http://streamlineanalytics.net:10000'
 
-def send_post_request(url, data):
+def send_command(ws, data):
     encoded_data = json.dumps(data).encode('utf-8')
-    request = urllib.request.Request(url, data=encoded_data, method='POST')
-    request.add_header('Content-Type', 'application/json')
-    with urllib.request.urlopen(request) as response:
-        response_data = response.read().decode('utf-8')
-    return response_data
+    ws.send_text(encoded_data)
 
-app_name = None
-while True:
-    app_name = input('app name: ').strip()
-    if len(app_name) == 0:
-        print('Invalid app name')
-        continue
-    break
+def on_message(ws, message):
+    pass
 
-while True:
-    command = input('command: ').strip()
-    if len(command) == 0:
-        print('Invalid command')
-        continue
-    url = f'{SERVER_URL}/cmd'
-    data = { 'text': f'{app_name} {command}' }
-    response = send_post_request(url, data)
-    print('sent')
-    print()
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+def on_open(ws):
+    print("Opened connection\n")
+    app_name = None
+    while True:
+        app_name = input('app name: ').strip()
+        if len(app_name) == 0:
+            print('Invalid app name')
+            exit(0)
+        break
+
+    while True:
+        command = input('command: ').strip()
+        if len(command) == 0:
+            print('Invalid command')
+            exit(0)
+        data = { 'type': 'cmd', 'room': app_name, 'command': command }
+        send_command(ws, data)
+        print('sent\n')
+
+if __name__ == "__main__":
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp(f"wss://streamlineanalytics.net:10001",
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}, dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
