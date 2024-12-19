c27c7d2805971e0652a8a0f92b724b287514e9ef
mac <mac@macs-MacBook-Pro-3.local>
2024-12-19 03:10:55 -0500

    auto reconnect

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import websocket
 import ssl
 import rel
 import json
+import time
 
 SERVER_URL = 'http://streamlineanalytics.net:10000'
+
+ws = None
 
 def send_command(ws, data):
     encoded_data = json.dumps(data).encode('utf-8')
@@ -14,9 +17,13 @@ def on_message(ws, message):
 
 def on_error(ws, error):
     print(error)
+    time.sleep(2)
+    reconnect()
 
 def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
+    time.sleep(2)
+    reconnect()
 
 def on_open(ws):
     print("Opened connection\n")
@@ -37,14 +44,18 @@ def on_open(ws):
         send_command(ws, data)
         print('sent\n')
 
-if __name__ == "__main__":
-    websocket.enableTrace(False)
+def reconnect():
+    global ws
     ws = websocket.WebSocketApp(f"wss://streamlineanalytics.net:10001",
                               on_open=on_open,
                               on_message=on_message,
                               on_error=on_error,
                               on_close=on_close)
 
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}, dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}, dispatcher=rel, reconnect=5)
+
+if __name__ == "__main__":
+    websocket.enableTrace(False)
+    reconnect()
     rel.signal(2, rel.abort)  # Keyboard Interrupt
     rel.dispatch()
