5dda45e90088941bdd95005d0392a04dff301430
mac <mac@macs-MacBook-Pro-3.local>
2024-12-20 15:10:38 -0500

    auto reconnect

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import ssl
 import rel
 import json
 import time
+import threading
 
 SERVER_URL = 'http://streamlineanalytics.net:10000'
 
@@ -11,39 +12,26 @@ ws = None
 def send_command(data):
     global ws
     encoded_data = json.dumps(data).encode('utf-8')
-    ws.send_text(encoded_data)
+    try:
+        ws.send_text(encoded_data)
+    except:
+        print('Error while sending command')
 
 def on_message(ws, message):
     pass
 
 def on_error(ws, error):
     print(error)
-    time.sleep(2)
-    reconnect()
 
 def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
-    time.sleep(2)
-    reconnect()
 
 def on_open(_ws):
     global ws
     print("Opened connection\n")
     ws = _ws
 
-def reconnect():
-    ws = websocket.WebSocketApp(f"wss://streamlineanalytics.net:10001",
-                              on_open=on_open,
-                              on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
-
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}, dispatcher=rel, reconnect=5, ping_interval=10)
-
-if __name__ == "__main__":
-    websocket.enableTrace(False)
-    reconnect()
-
+def input_thread():
     app_name = None
     while True:
         app_name = input('app name: ').strip()
@@ -60,6 +48,19 @@ if __name__ == "__main__":
         data = { 'type': 'cmd', 'room': app_name, 'command': command }
         send_command(data)
         print('sent\n')
+
+if __name__ == "__main__":
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp(f"wss://streamlineanalytics.net:10001",
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}, dispatcher=rel, reconnect=5, ping_interval=10)
+
+    input_handler = threading.Thread(target=input_thread)
+    input_handler.start()
 
     rel.signal(2, rel.abort)  # Keyboard Interrupt
     rel.dispatch()
